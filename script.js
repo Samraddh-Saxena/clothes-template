dfa013a2009cffcadfa1876e52bf0b1b3deb2272
Samraddh-Saxena <samraddhsaxena@gmail.com>
2023-08-05 21:25:02 +0530

    updated

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.footer-list').classList.toggle('show-menu');
     document.querySelector('.sidenav').classList.toggle('show-sidenav');
   });
+
+  function change() {
+    var x = document.getElementById("subL");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
